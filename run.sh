1af1f8b1455b1a93d574e9adc5b8931181383a0b
Ruber Flores <ruberfc@gmail.com>
2022-12-30 22:17:41 -0500

    Cuarto commit

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-docker stop api-gateway && docker rm api-gateway
+docker stop php-app && docker rm php-app
 
-docker image rm api-gateway
+docker image rm php-app
 
-docker build -t api-gateway .
+docker build -t php-app .
 
-docker run -d --name api-gateway --net=my-net -p 8000:80 api-gateway
+docker run -d --name php-app --net=my-net -p 8000:80 php-app
